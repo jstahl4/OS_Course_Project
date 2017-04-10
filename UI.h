f108f645ca05e5c65b2849d56dbeb817a6f2a888
Asif Rahman <arahman@storm.cis.fordham.edu>
2017-04-10 16:30:54 -0400

    add UI class

---
+++ UI.h
@@ -0,0 +1,33 @@
+/**********************************************************************
+ * Asif Rahman - Team Coyote
+ **********************************************************************/
+#pragma once
+
+#include <string>
+#include <map>
+#include "atosAPI.h"
+
+class UI
+{
+	enum class Commands { Unknown, Create, Delete, Dir, Edit, Type, Exit };	// supported commands
+
+public:
+	UI();
+	~UI();
+
+	int run();
+
+private:
+	Commands					getCommand(std::string const& aLine) const;				// detetmine the command
+	void						createFile(std::string const& aLine);					// processing the createFile command.
+	void						deleteFile(std::string const& aLine);					// processing the deleteFile command.
+	void						editFile(std::string const& aLine);						// processing the editFile command.
+	void						typeFile(std::string const& aLine);						// processing the editFile command.
+	void						dir() const;											// print the directory list.
+	std::vector<std::string>	parseLine(std::string const& aLine) const;				// parse the command, and create separate strings
+	
+private:
+	std::map<Commands, std::string>	m_Commands;											// Assign commands id to the word of commmands.
+	int								m_FileHandle;
+};
+
