56e16d1a1b95cadcd12b896758b3b7faa155f54c
arahman13 <arahman13@fordham.edu>
2017-05-01 21:30:10 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,15 +1,32 @@
 /**********************************************************************
  * Team Coyote
- **********************************************************************/
+ **********************************************************************
+ */
 
 #include "UI.h"
-
 
 int main()
 {
 	//TODO: Create filesystem
+
+	// incorporate the filesystem.
+	// ... 
+	// FileSystem will incorporate the disk subsystem and file subsystem
+	// ...
+	// Intialize ATOS-FS API
+	// .. 
+	// ATOS-FS API now ready for work.
+
+	// incorporate UI
 	UI ui;
 
-	return ui.run();
+	// start UI - run from the main thread.
+	int res = ui.run();
+
+	// release resources (filesystem)
+	// ...
+	// resources freed.
+
+	return res;
 }
 
